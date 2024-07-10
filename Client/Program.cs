9e21a5b3083c9b7db52c19a810a24aa31fc976fd
filipw <filip@strathweb.com>
2024-07-10 19:59:42 +0200

    added .NET SSE sample

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.ServerSentEvents;
+using System.Text.Json;
+
+Console.WriteLine("Started!");
+
+using HttpClient client = new();
+using Stream stream = await client.GetStreamAsync("http://localhost:5068/weatherforecast");
+
+// as string
+//await foreach (SseItem<string> item in SseParser.Create(stream).EnumerateAsync())
+//{
+//    Console.WriteLine(item.Data);
+//}
+
+await foreach (SseItem<WeatherForecast?> item in SseParser.Create(stream, (eventType, bytes) => JsonSerializer.Deserialize<WeatherForecast>(bytes)).EnumerateAsync())
+{
+    if (item.Data != null)
+    {
+        Console.WriteLine($"Date: {item.Data.Date}, Temperature (in C): {item.Data.TemperatureC}, Summary: {item.Data.Summary}");
+    } 
+    else
+    {
+        Console.WriteLine("Couldn't deserialize the response");
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+}
---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Net.Http.Headers;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", async (HttpContext ctx) =>
+{
+    ctx.Response.Headers.Append(HeaderNames.ContentType, "text/event-stream");
+    while (!ctx.RequestAborted.IsCancellationRequested)
+    {
+        var forecast = new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(Random.Shared.Next(0, 8))),
+                Random.Shared.Next(-40, 50),
+                summaries[Random.Shared.Next(summaries.Length)]
+            );
+
+        await ctx.Response.WriteAsync($"data: ");
+        await JsonSerializer.SerializeAsync(ctx.Response.Body, forecast);
+        await ctx.Response.WriteAsync($"\n\n");
+        await ctx.Response.Body.FlushAsync();
+
+        await Task.Delay(2000);
+    }
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+}
