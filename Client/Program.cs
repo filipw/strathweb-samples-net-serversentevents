1364f014a422841e266bb13528b7251b063d2b28
filipw <filip@strathweb.com>
2024-07-11 17:04:52 +0200

    added example with multiple events

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,26 +1,63 @@
 ﻿using System.Net.ServerSentEvents;
 using System.Text.Json;
 
-using var client = new HttpClient();
-using var stream = await client.GetStreamAsync("http://localhost:5068/weatherforecast");
+//await Example1String();
+//await Example2TypedSingleEventType();
+await Example3TypedMultipleEventType();
 
-// option 1: as string
-//await foreach (SseItem<string> item in SseParser.Create(stream).EnumerateAsync())
-//{
-//    Console.WriteLine(item.Data);
-//}
-
-// option 2: typed
-await foreach (SseItem<WeatherForecast?> item in SseParser.Create(stream, (eventType, bytes) => JsonSerializer.Deserialize<WeatherForecast>(bytes)).EnumerateAsync())
+async Task Example1String()
 {
-    if (item.Data != null)
+    using var client = new HttpClient();
+    using var stream = await client.GetStreamAsync("http://localhost:5068/weatherforecast");
+    await foreach (SseItem<string> item in SseParser.Create(stream).EnumerateAsync())
     {
-        Console.WriteLine($"Date: {item.Data.Date}, Temperature (in C): {item.Data.TemperatureC}, Summary: {item.Data.Summary}");
-    } 
-    else
+        Console.WriteLine(item.Data);
+    }
+}
+
+async Task Example2TypedSingleEventType()
+{
+    using var client = new HttpClient();
+    using var stream = await client.GetStreamAsync("http://localhost:5068/weatherforecast");
+    await foreach (SseItem<WeatherForecast?> item in SseParser.Create(stream, (eventType, bytes) => 
+        JsonSerializer.Deserialize<WeatherForecast>(bytes)).EnumerateAsync())
     {
-        Console.WriteLine("Couldn't deserialize the response");
+        if (item.Data != null)
+        {
+            Console.WriteLine($"Date: {item.Data.Date}, Temperature (in C): {item.Data.TemperatureC}, Summary: {item.Data.Summary}");
+        }
+        else
+        {
+            Console.WriteLine("Couldn't deserialize the response");
+        }
+    }
+}
+
+async Task Example3TypedMultipleEventType()
+{
+    using var client = new HttpClient();
+    using var stream = await client.GetStreamAsync("http://localhost:5068/weatherforecast-or-sportscore");
+    await foreach (var item in SseParser.Create(stream, (eventType, bytes) => eventType switch
+    {
+        "WeatherForecast" => JsonSerializer.Deserialize<WeatherForecast>(bytes),
+        "SportScore" => JsonSerializer.Deserialize<SportScore>(bytes) as object,
+        _ => null
+    }).EnumerateAsync())
+    {
+        switch (item.Data)
+        {
+            case WeatherForecast weatherForecast:
+                Console.WriteLine($"Date: {weatherForecast.Date}, Temperature (in C): {weatherForecast.TemperatureC}, Summary: {weatherForecast.Summary}");
+                break;
+            case SportScore sportScore:
+                Console.WriteLine($"Team 1 vs Team 2 {sportScore.Team1Score}:{sportScore.Team2Score}");
+                break;
+            default:
+                Console.WriteLine("Couldn't deserialize the response");
+                break;
+        }
     }
 }
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
+record SportScore(int Team1Score, int Team2Score);
--- Server/Program.cs
+++ Server/Program.cs
@@ -32,6 +32,43 @@ app.MapGet("/weatherforecast", async (HttpContext ctx) =>
     }
 });
 
+app.MapGet("/weatherforecast-or-sportscore", async (HttpContext ctx) =>
+{
+    ctx.Response.Headers.Append(HeaderNames.ContentType, "text/event-stream");
+    while (!ctx.RequestAborted.IsCancellationRequested)
+    {
+        if (Random.Shared.Next(2) == 0)
+        {
+            var forecast = new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(Random.Shared.Next(8))),
+                    Random.Shared.Next(-40, 50),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                );
+
+            await WriteEvent(ctx, "WeatherForecast", forecast);
+        } 
+        else
+        {
+            var score = new SportScore(Random.Shared.Next(10), Random.Shared.Next(10));
+            await WriteEvent(ctx, "SportScore", score);
+        }
+
+        await Task.Delay(2000);
+    }
+});
+
+async Task WriteEvent<T>(HttpContext ctx, string eventName, T data)
+{
+    await ctx.Response.WriteAsync($"event: {eventName}\n");
+    await ctx.Response.WriteAsync($"data: ");
+    await JsonSerializer.SerializeAsync(ctx.Response.Body, data);
+    await ctx.Response.WriteAsync($"\n\n");
+    await ctx.Response.Body.FlushAsync();
+}
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
+
+record SportScore(int Team1Score, int Team2Score);
